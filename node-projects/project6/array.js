6d167ccac56bc496f7bff3ebc04fd81b19005af5
Ubuntu <labuser@ubuntu2204.k0ygzfdyhlkefd3jyoipqobu5h.rx.internal.cloudapp.net>
2024-07-12 10:30:12 +0000

    Updated files for projects backend

---
+++ node-projects/project6/array.js
@@ -0,0 +1,7 @@
+// Declare a simple array
+let nameArray = ['John', 'Sam', 'Max'];
+console.log(nameArray);
+
+// Push a new value to the array
+nameArray.push('Roy');
+console.log(nameArray);
--- node-projects/project6/index.js
+++ node-projects/project6/index.js
@@ -1,5 +1,18 @@
-const greetUser = function(username){
+// Simple function creation
+function greetUser(username){
+    console.log(">>> Hello", username, '\b, How are you?');
+}
+
+// Store function reference in a variable
+const greetUser1 = function(username){
+    console.log(">>> Hello", username, '\b, How are you?');
+}
+
+// Declare first class function using arrow operator
+const greetUser2 = (username) => {
     console.log(">>> Hello", username, '\b, How are you?');
 }
 
 greetUser("John");
+greetUser("Sam");
+greetUser("Ace");
